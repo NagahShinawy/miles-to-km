68ad0e640dacd9295876ca9c9131f9595960a534
Nagah <nagah.shinawy@gmail.com>
2021-06-28 09:23:28 +0300

    checkbox, spinbox, text, radio, listbox, scale

--- constants.py
+++ constants.py
@@ -5,6 +5,7 @@ created by Nagaj at 26/06/2021
 MILES_TO_KM = "Miles to Kilometers"
 IMAGE_TITLE_BAR = "./km-to-miles.ico"
 WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 750
 TOP = "top"
 MAIN_TITLE_XPAD = 30
 MAIN_TITLE_YPAD = 60
--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 created by Nagaj at 26/06/2021
 """
 import random
-from tkinter import *
 from functools import partial
+from tkinter import *
+
 from config import setup
 from constants import *
 
@@ -40,7 +41,11 @@ def main():
     :return:
     """
     window = setup(
-        title=MILES_TO_KM, image=IMAGE_TITLE_BAR, width=WINDOW_WIDTH, is_center=True
+        title=MILES_TO_KM,
+        image=IMAGE_TITLE_BAR,
+        width=WINDOW_WIDTH,
+        height=WINDOW_HEIGHT,
+        is_center=True,
     )
 
     miles_to_km = Label(text="Convert Miles to Km", font=("Arial", 15, "bold"))
@@ -77,6 +82,83 @@ def main():
     click_me.pack(side="top")
 
     ############################
+    # Text
+    text = Text(height=5, width=30)
+    # Puts cursor in textbox. to be ready for writing
+    text.focus()
+    # Adds some text to begin with.
+    text.insert(END, "Example of multi-line text entry.")
+    text.insert(CURRENT, "test.")
+    # Get's current value in textbox at line 1, character 0
+    # 1.5 ==> means from line#1 charIndex#5
+    print(text.get("1.5", END))
+    text.pack()
+
+    ###########################
+    # Spinbox
+    def spinbox_used():
+        # gets the current value in spinbox.
+        print(spinbox.get())
+
+    spinbox = Spinbox(from_=1, to=120, width=5, command=spinbox_used)
+    spinbox.pack()
+
+    ##########################
+    # Scale
+    # Called with current scale value.No need to parse it
+    def scale_used(value):
+        print(value)
+
+    scale = Scale(from_=1, to=100, command=scale_used)
+    scale.pack()
+
+    ##########################
+
+    # Checkbutton
+    def checkbutton_used():
+        # Prints 1 if On button checked, otherwise 0.
+        print("Value is", checked_state.get())
+
+    # variable to hold on to checked state, 0 is off, 1 is on.
+    checked_state = IntVar()
+    # checked_state.set(0
+    checkbutton = Checkbutton(
+        text="Is On?", variable=checked_state, command=checkbutton_used
+    )
+    print("V is", checked_state.get())
+    checkbutton.pack()
+    #########################
+    print("#" * 100)
+
+    # Radiobutton
+    def radio_used():
+        print(radio_state.get())
+
+    # Variable to hold on to which radio button value is checked.
+    radio_state = IntVar()
+    radiobutton1 = Radiobutton(
+        text="Option1", value=100, variable=radio_state, command=radio_used
+    )
+    radiobutton2 = Radiobutton(
+        text="Option2", value=200, variable=radio_state, command=radio_used
+    )
+    print(radio_state.get())  # default value is 0
+    radiobutton1.pack()
+    radiobutton2.pack()
+
+    #########################
+    def listbox_used(event):
+        # Gets current selection from listbox
+        print(listbox.get(listbox.curselection()))
+
+    listbox = Listbox(height=4)
+    fruits = ["Apple", "Pear", "Orange", "Banana"][::-1]
+    for fruit in fruits:
+        index = fruits.index(fruit)
+        listbox.insert(index, fruit)
+    listbox.bind("<<ListboxSelect>>", listbox_used)
+    listbox.pack()
+    #########################
     window.mainloop()  # keep window open while working
 
 
